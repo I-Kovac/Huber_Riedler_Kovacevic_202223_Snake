441f7b0c27ec285b480baedc99db63e7817f2131
jhuber <jhuber@htl-steyr.ac.at>
2022-10-19 10:16:14 +0200

    MENU

--- src/main/java/com/example/huber_riedler_kovacevic_202223_snake/HelloApplication.java
+++ src/main/java/com/example/huber_riedler_kovacevic_202223_snake/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Menu");
         stage.setScene(scene);
         stage.show();
     }
